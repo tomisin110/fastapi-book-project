c31ada4f382ac67a6b44375ee5ef97e616aad88d
tomisin110 <ayoemma188@gmail.com>
2025-02-11 14:39:13 +0100

    fix: Resolve api router by returning prefix back to former

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,10 +1,11 @@
+from typing import OrderedDict
+
 from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
-from typing import OrderedDict
+
 from api.db.schemas import Book, Genre, InMemoryDB
 
-# Initialize the router with the correct prefix
-router = APIRouter(prefix="/api/v1/books", tags=["books"])
+router = APIRouter()
 
 db = InMemoryDB()
 db.books = {
@@ -31,6 +32,7 @@ db.books = {
     ),
 }
 
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     db.add_book(book)
@@ -38,9 +40,13 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
-@router.get("/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK)
+
+@router.get(
+    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
+)
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
@@ -48,6 +54,7 @@ async def update_book(book_id: int, book: Book) -> Book:
         status_code=status.HTTP_200_OK,
         content=db.update_book(book_id, book).model_dump(),
     )
+
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
@@ -63,3 +70,7 @@ async def get_book(book_id: int) -> Book:
             detail="Book not found",
         )
     return book
+
+
+
+
